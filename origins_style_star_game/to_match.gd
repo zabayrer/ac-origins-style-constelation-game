2447edc47f30d73d13573b0bb560b87f3cc2cd84
zabayrer <zabayrer@s.sfusd.edu>
2024-06-20 22:36:00 -0700

    made to_match the moving constelation

--- origins_style_star_game/to_match.gd
+++ origins_style_star_game/to_match.gd
@@ -13,3 +13,22 @@ func _process(delta):
 		rotation += 1 * delta
 	if Input.is_action_pressed("rotate_ccw"):
 		rotation += -1 * delta
+	
+	var velocity = Vector2.ZERO
+
+	#change velocity by player's input
+	if Input.is_action_pressed("ui_up"):
+		velocity.y += -10
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 10
+	if Input.is_action_pressed("ui_left"):
+		velocity.x += -10
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 10
+
+	#normalize the velocity
+	velocity = velocity.normalized()
+
+	#move screen
+	position += velocity * delta * 100
+
